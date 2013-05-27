ad997aadf1ce1c4d54ac25237b573f02fcf94315
cody <git@codylambert.com>
2013-05-27 16:58:20 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ooshkaboosh::Application.config.secret_token = '3365cfdb5120347e18391fdc3f62e723064237c0b0d32449c7c770da92ce02bb858d5916be3c0060502ab0bad271b32e2fdeb5a555b9c5171edce3376a473876'
